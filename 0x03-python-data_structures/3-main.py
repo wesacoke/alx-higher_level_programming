16389c6e623ba8fe52172ead291fefb0b8848a00
wesacoke <princewsa@gmail.com>
2024-01-09 03:34:14 -0800

    python data structures

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,6 @@
+guillaume@ubuntu:~/0x03$ cat 3-main.py
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
