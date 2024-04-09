76cb16021925839f5ba3a9ba482abf8b8e0565c0
wesacoke <wesaemmanuel02@gmail.com>
2024-04-09 13:36:50 -0700

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 
 class Rectangle {
   construtor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 }
-
 module.exports = Rectangle;
