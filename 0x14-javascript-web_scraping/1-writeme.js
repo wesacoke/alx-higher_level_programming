7ef155351af6176d6e18bdf92ab0fee3c8797c20
wesacoke <wesaemmanuel02@gmail.com>
2024-05-27 12:37:05 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
-	function (err) {
-		if (err) {
-			console.log(err);
-		}
-	});
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
