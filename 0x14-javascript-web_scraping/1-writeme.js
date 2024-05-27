e5db47351bfca3180fb374e4f6b820c3ca5e509e
wesacoke <wesaemmanuel02@gmail.com>
2024-05-27 12:00:09 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.wirterFile(process.argv[2], process.argv[3], 'utf-8',
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
 	fuction (err) {
 		if (err) {
 			console.log(err);
