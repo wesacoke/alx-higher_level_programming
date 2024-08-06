f4273d77eac7eaf0a53e9c3f98db802f24c396d9
wesacoke <wesaemmanuel02@gmail.com>
2024-08-06 12:32:20 +0300

    Python Networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #This script takes in a URL, sends a request to the URL and display size of the body response in bytes
-curl -Is "$!" | grep -i "content-Length" | cut -d' ' -f2
+curl -s "$1" | wc -c
