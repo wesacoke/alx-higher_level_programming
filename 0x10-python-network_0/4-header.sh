7a92e206a6ba0e346d44183ba426b359dc4b848a
wesacoke <wesaemmanuel02@gmail.com>
2024-08-06 13:56:37 +0300

    Python Networking

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to the URL with the X-School-Id header and display the body of the response
-curl -SH "X-School-user-Id: 9" "$1"
+# Send a GET request to the URL with the X-School-Id header
+curl - SH "X-School-user-Id: 98" "$1"
