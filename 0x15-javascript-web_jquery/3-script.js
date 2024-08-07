1a4b9eea29ae3099617940299b9e7d14c13c685d
wesacoke <wesaemmanuel02@gmail.com>
2024-08-07 12:27:46 +0300

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+$(document).ready(FUNCTION () {
+    //Code is executed only when the document is fullly loaded
+    $('#red_header').click(function () {
+        $('header').addClass(red);
+    });
+});
